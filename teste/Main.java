6fbf5a1bb914d03f97cc9e4450e2fae2b3852b4d
Caio Rodrigo Galiza Salin de Melo <caiorodrigou@gmail.com>
2025-09-24 03:24:44 +0000

    corrijir o aumentar array nos pushs

--- teste/Main.java
+++ teste/Main.java
@@ -8,12 +8,14 @@ public class Main {
     public void menu(){
         System.out.println();
         System.out.println("ESCOLHA UMA OPÇÃO");
-        System.out.println("1 - PUSH");
-        System.out.println("2 - POP");
-        System.out.println("3 - TOP");
-        System.out.println("4 - SIZE");
-        System.out.println("5 - ISEMPTY");
-        System.out.println("6 - sair");
+        System.out.println("1 - Push Vermelho / 6 - Push Preto");
+        System.out.println("2 - Pop Vermelho / 7 - Pop Preto");
+        System.out.println("3 - Top Vermelho / 8 - Top Preto");
+        System.out.println("4 - Size Vermelho / 9 - Size Preto");
+        System.out.println("5 - IsEmpty Vermelho / 10 - IsEmpty Preto");
+        System.out.println();
+        System.out.println("11 - sair");
+        System.out.println();
         System.out.println("Qual operação deseja fazer?");
         System.out.println();
 
@@ -24,40 +26,50 @@ public class Main {
                 System.out.println();
                 System.out.println("Digite o valor que deseja adicionar");
                 int valor = imput.nextInt();
-                pilha.push(valor);
+                pilha.push_vermelho(valor);
                 pilha.print();
                 menu();
             }
     
             case 2 -> {
-                pilha.pop();
+                pilha.pop_vermelho();
                 pilha.print();
                 menu();
             }
             case 3 -> {
                 System.out.println();
-                System.out.println("topo da pilha :" + pilha.top());
+                System.out.println("topo da pilha :" + pilha.top_vermelho());
                 menu();
             }   
 
             case 4 -> {
                 System.out.println();
-                System.out.println("tamanho da pilha :" + pilha.size());
+                System.out.println("tamanho da pilha :" + pilha.size_vermelho());
                 pilha.print();
                 menu();
             }
 
             case 5 -> {
                 System.out.println();
-                pilha.isEmpty();
+                pilha.isEmpty_vermelho();
                 System.out.println();
                 menu();
             }   
 
             case 6 -> {
+                System.out.println();
+                System.out.println("Digite o valor que deseja adicionar");
+                int valor = imput.nextInt();
+                pilha.push_preto(valor);
+                pilha.print();
+                menu();
+            }
+
+            case 11 -> {
                 System.out.println("Saindo...");
                 break;
             }
+            
             default -> {
                 System.out.println("Opção inválida. Tente novamente.");
                 menu();
--- teste/Metodos.java
+++ teste/Metodos.java
@@ -4,12 +4,14 @@ public class Metodos {
     
     private int capacidade;
     private Object[] itens;
-    private int t;
+    private int t_v;
+    private int t_p;    
     private int FC;
 
     public Metodos (int capacidade, int crescimento){
         this.capacidade = capacidade;
-        this.t = -1;
+        this.t_v = -1;
+        this.t_p = capacidade;
         this.FC = crescimento;
         if (crescimento <= 0){
             this.FC = 0;
@@ -17,12 +19,15 @@ public class Metodos {
         itens =  new Object[capacidade];
     }
     
-    public void push (Object o){
-        if (t == itens.length - 1){
+    public void push_vermelho (Object o){
+        if (t_v ==  t_p - 1){
             if (FC > 0){
                 Object[] novo = new Object[itens.length + FC];
-                for (int i = 0; i < itens.length; i++){
+                for (int i = 0; i < t_v; i++){
                     novo[i] = itens[i];
+                }
+                for (int i = capacidade - 1; i < t_p; i--){
+                    novo[capacidade - i] = itens[capacidade - i];
                 }
                 this.capacidade = novo.length;
                 itens = novo;
@@ -39,47 +44,110 @@ public class Metodos {
             }
         }
 
-        t = t + 1;
-        itens[t] = o;
+        t_v = t_v + 1;
+        itens[t_v] = o;
            
     }
 
-    public Object pop () throws PilhaVaziaExcecao {
-        if (t == -1){
+    public Object pop_vermelho () throws PilhaVaziaExcecao {
+        if (t_v == -1){
             System.out.println();
             System.out.println("Pilha está vazia. Não é possível remover mais elementos.");
             return null;
         } else {
-            t = t - 1;
+            t_v = t_v - 1;
         }
-        Object removido = itens[t + 1];
+        Object removido = itens[t_v + 1];
 
         System.out.println(" Elemento removido: " + removido);
 
         return removido;
     }
 
-    public Object top () throws PilhaVaziaExcecao {
-        if (t == -1){
+    public Object top_vermelho () throws PilhaVaziaExcecao {
+        if (t_v == -1){
             System.out.println("Pilha está vazia. Não é possível ver o topo da pilha.");
             return null;
         }
-        return itens[t];
+        return itens[t_v];
     }
 
-    public int size (){
-        return t + 1;
+    public int size_vermelho (){
+        return t_v + 1;
     }
 
-    public boolean isEmpty(){
-        return t==-1;
+    public boolean isEmpty_vermelho(){
+        return t_v == -1;
+    }
+    //                                 pilha preta
+    //----------------------------------------------------------------------------------------------------------------
+
+    public void push_preto (Object o){
+        if (t_p == t_v + 1 ){
+            if (FC > 0){
+                Object[] novo = new Object[itens.length + FC];
+                for (int i = 0; i < t_v; i++){
+                    novo[i] = itens[i];
+                }
+                for (int i = capacidade - 1; i < t_p; i--){
+                    novo[capacidade - i] = itens[capacidade - i];
+                }
+                this.capacidade = novo.length;
+                itens = novo;
+            } else if (FC == 0) {
+                Object[] novo = new Object[itens.length + itens.length];
+                for (int i = 0; i < itens.length; i++){
+                    novo[i] = itens[i];
+                }
+                this.capacidade = novo.length;
+                itens = novo;
+            } else {
+                System.out.println("Pilha está cheia. Não é possível adicionar mais elementos.");
+                return;
+            }
+        }
+
+        t_p = t_p - 1;
+        itens[t_p] = o;
+           
+    }
+
+    public Object pop_preto () throws PilhaVaziaExcecao {
+        if (t_p == -1){
+            System.out.println();
+            System.out.println("Pilha está vazia. Não é possível remover mais elementos.");
+            return null;
+        } else {
+            t_p = t_p - 1;
+        }
+        Object removido = itens[t_p + 1];
+
+        System.out.println(" Elemento removido: " + removido);
+
+        return removido;
+    }
+
+    public Object top_preto () throws PilhaVaziaExcecao {
+        if (t_p == -1){
+            System.out.println("Pilha está vazia. Não é possível ver o topo da pilha.");
+            return null;
+        }
+        return itens[t_p];
+    }
+
+    public int size_preto (){
+        return t_p + 1;
+    }
+
+    public boolean isEmpty_preto(){
+        return t_p == -1;
     }
 
     public void print (){
         System.out.println();
         System.out.println("----------------------------------");
         System.out.print("Pilha: ");
-        for (int i = 0; i <= t; i++){
+        for (int i = 0; i <= capacidade - 1; i++){
             System.out.print(itens[i] + " ");
         }
         System.out.print("  /  ");
