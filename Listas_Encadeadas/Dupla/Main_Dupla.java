551123f9420bf8f520201fc57d6ab91994b2b9a3
Caio Rodrigo Galiza Salin de Melo <caiorodrigou@gmail.com>
2025-10-08 15:46:45 -0300

    Update Main_Dupla.java

--- Listas_Encadeadas/Dupla/Main_Dupla.java
+++ Listas_Encadeadas/Dupla/Main_Dupla.java
@@ -1,5 +1,113 @@
 package Listas_Encadeadas.Dupla;
+import java.util.Scanner;
 
 public class Main_Dupla {
-    
+
+    Scanner imput = new Scanner(System.in);
+    Deque_Duplamente_encadeado Deque = new Deque_Duplamente_encadeado();
+
+    public void menu() {
+
+        System.out.println();
+        System.out.println("-------------------------------");
+        System.out.println("ESCOLHA UMA OPÇÃO");
+        System.out.println("1 - adicionar inicio");
+        System.out.println("2 - remover inicio");
+        System.out.println("3 - adicionar final");
+        System.out.println("4 - remover final");
+        System.out.println("5 - primeiro");
+        System.out.println("6 - ultimo");
+        System.out.println("7 - verificar se esta vazia");
+        System.out.println("8 - tamanho");
+        System.out.println("9 - sair");
+        System.out.println("-------------------------------");
+        System.out.println();
+
+        int op = imput.nextInt();
+        switch (op) {
+
+            case 1: {
+                System.out.println();
+                System.out.println("Digite o valor que deseja adicionar");
+                int valor = imput.nextInt();
+                Deque.Adicionar_I(valor);
+                Deque.print();
+                menu();
+
+            }
+
+            case 2: {
+
+                System.out.println("Valor removido: " + Deque.Remover_I());
+                Deque.print();
+                menu();
+
+            }
+
+            case 3: {
+                System.out.println();
+                System.out.println("Digite o valor que deseja adicionar");
+                int valor = imput.nextInt();
+                Deque.Adicionar_F(valor);
+                Deque.print();
+                menu();
+
+            }
+
+            case 4: {
+
+                System.out.println("Valor removido: " + Deque.Remover_F());
+                Deque.print();
+                menu();
+
+            }
+
+            case 5: {
+                
+                System.out.println("Inicio: " + Deque.Inicio());
+                menu();
+
+            }
+
+            case 6: {
+                
+                System.out.println("Fim: " + Deque.Fim());
+                menu();
+
+            }
+
+            case 7: {
+
+                if (Deque.Vazia()) {
+                    System.out.println("Deque está vazio");
+                } else {
+                    System.out.println("Deque não está vazio");
+                }   
+                menu();
+
+            }
+
+            case 8: {
+
+                System.out.println("Tamanho: " + Deque.Tamanho());
+                menu();
+
+            }
+
+            case 9: {
+                System.out.println("Tchau!");
+                break;
+            }
+
+        }
+    }
+    public static void main(String[] args) {
+
+        Main_Dupla main = new Main_Dupla();
+        main.menu();
+
+    }
 }
+
+    
+
